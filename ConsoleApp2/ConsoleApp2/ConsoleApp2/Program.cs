9a6f8346d25ffc27b4d8a4edcbd1f3d7089256e0
Charloteaux Anne-Laure <annelaure.charloteaux@viacesi.fr>
2025-03-24 16:07:04 +0100

    question1

--- ConsoleApp2/ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,15 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System;
+﻿using System;
 
 class Program
 {
     static void Main(string[] args)
     {
         string[] mois = { "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre" };
-        
+
         Console.Write("Entrez votre salaire mensuel : ");
         double salaireMensuel = double.Parse(Console.ReadLine());
+        
+        Console.Write("Entrez le pourcentage de votre prime de Noël (exemple : 10 pour 10%) : ");
+        int pourcentagePrime = int.Parse(Console.ReadLine());
+
 
         for (int i = 0; i < mois.Length; i++)
         {
@@ -18,19 +20,20 @@ class Program
                 Console.WriteLine($"{mois[i]} : Entreprise fermée");
                 continue;
             }
-            
+
+            double salaireAffiche = salaireMensuel;
+
             if (mois[i] == "Décembre")
             {
-                double prime = salaireMensuel * 11 * 0.10;
-                salaireMensuel += prime;
-                Console.WriteLine($"{mois[i]} : {salaireMensuel} € (avec prime de {prime} € pour le mois de décembre)");
+                double prime = salaireMensuel * 11 * 0.10;  
+                salaireAffiche += prime;
+
+                Console.WriteLine($"{mois[i]} : {salaireAffiche} € (+ 10% du salaire annuel : {prime} €)");
             }
             else
             {
-                Console.WriteLine($"{mois[i]} : {salaireMensuel} €");
+                Console.WriteLine($"{mois[i]} : {salaireAffiche} €");
             }
-
-         
         }
     }
 }
