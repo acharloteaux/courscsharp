40b428e2c326cc03cb05b6e54c0edb5b430df879
Charloteaux Anne-Laure <annelaure.charloteaux@viacesi.fr>
2025-03-24 11:28:45 +0100

    deplacement du projet

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Étape 1 : calcul du salaire brut mensuel à partir de 30k annuel
+        double salaireAnnuel = 30000;
+        double salaireMensuel = salaireAnnuel / 12;
+
+        Console.WriteLine($"Le salaire brut mensuel est : {salaireMensuel} €");
+    }
+}
