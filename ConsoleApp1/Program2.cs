5c33ef45249d8177a807e7b752dd2fcdc7a4aae2
Charloteaux Anne-Laure <annelaure.charloteaux@viacesi.fr>
2025-03-24 15:14:01 +0100

    commit program2

--- ConsoleApp1/Program2.cs
+++ ConsoleApp1/Program2.cs
@@ -1,28 +1,6 @@
-﻿using System;
+﻿namespace ConsoleApp1;
 
-class Program2
+public class Program2
 {
-    public static void Run()
-    {
-        // Demande à l'utilisateur son salaire brut annuel
-        Console.Write("Entrez votre salaire brut annuel (€) : ");
-        double salaireAnnuelBrut = Convert.ToDouble(Console.ReadLine());
-
-        // Demande à l'utilisateur le taux d'imposition
-        Console.Write("Entrez votre taux d'imposition (en %) : ");
-        double tauxImpositionPourcentage = Convert.ToDouble(Console.ReadLine());
-
-        // Conversion du pourcentage en décimal
-        double tauxImposition = tauxImpositionPourcentage / 100;
-
-        // Calcul du salaire net annuel
-        double salaireAnnuelNet = salaireAnnuelBrut * (1 - tauxImposition);
-
-        // Calcul du salaire net mensuel
-        double salaireNetMensuel = salaireAnnuelNet / 12;
-
-        // Affichages
-        Console.WriteLine($"\nLe salaire brut annuel est : {salaireAnnuelBrut} €");
-        Console.WriteLine($"Le salaire net mensuel est : {salaireNetMensuel:N2} €");
-    }
+    
 }
---
+++ ConsoleApp2/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] mois = { "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre" };
+        
+        Console.Write("Entrez votre salaire mensuel : ");
+        double salaireMensuel = double.Parse(Console.ReadLine());
+
+        for (int i = 0; i < mois.Length; i++)
+        {
+            if (mois[i] == "Aout")
+            {
+                Console.WriteLine($"{mois[i]} : Entreprise fermée");
+                continue;
+            }
+            
+            if (mois[i] == "Décembre")
+            {
+                double prime = salaireMensuel * 11 * 0.10;
+                salaireMensuel += prime;
+                Console.WriteLine($"{mois[i]} : {salaireMensuel} € (avec prime de {prime} € pour le mois de décembre)");
+            }
+            else
+            {
+                Console.WriteLine($"{mois[i]} : {salaireMensuel} €");
+            }
+
+         
+        }
+    }
+}
